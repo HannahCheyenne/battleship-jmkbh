dea8adb2aca5762a2746f28412dc3d8ff4111f3d
Jackie Abert <jax.abert@gmail.com>
2020-10-14 19:01:41 -0400

    test files for components

--- src/Components/Buttons/DemoGame/DemoGame.js
+++ src/Components/Buttons/DemoGame/DemoGame.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 import './demogame.css'
 //this component will join the demo version of the game against the ai
 class DemoGame extends Component{
     render(){
         return(
-            <button>Demo Game</button>
+            <Link to="/demo"><button>Demo Game</button></Link>
         )
     }
 }
