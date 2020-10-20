42a88fd32ea64bbc19cc04ae524df2cc3961ea30
Jackie Abert <jax.abert@gmail.com>
2020-10-20 10:37:15 -0400

    parent child state rendering solution needed

--- src/Components/BoardRender/BoardRender.js
+++ src/Components/BoardRender/BoardRender.js
@@ -1,33 +1,50 @@
 import React, { Component } from "react";
+import boom from '../../Images/boom.png'
+import miss from '../../Images/miss.png'
 import './boardrender.css'
 
 export default class Board extends Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+          id: ''
+        };
+      }
+      //child
+      handleClick(event) {
+        this.setState({
+          id: event.target.id
+        },this.props.playerMove(this.state.id));
+    
+      }
     //for now the game size is defaulted at 8x8. need to refactor this so it
     //so we can change the size of the grid. shouldn't be too hard. stretch goal!
-
   render() {
       const { test } = this.props
-
+      const H = <img className ="image" src={boom} alt="hit"/>
+        const M = <img className= 'image' src={miss} alt="miss"/>
+        console.log("click id in board render", this.state.id)
     return (<>
       <div className="boardContainer">
         <div className="board">
-            {test[0].map((i, index) => (<button key={`0.${index}`} id={`0.${index}`} value={i} className="slot" >
-                {i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
-            {test[1].map((i, index) => (<button key={`1.${index}`} id={`1.${index}`} value={i} className="slot" >{i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
-            {test[2].map((i, index) => (<button key={`2.${index}`} id={`2.${index}`} value={i} className="slot" >{i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
-            {test[3].map((i, index) => (<button key={`3.${index}`} id={`3.${index}`} value={i} className="slot" >{i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
-            {test[4].map((i, index) => (<button key={`4.${index}`} id={`4.${index}`} value={i} className="slot" >{i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
-            {test[5].map((i, index) => (<button key={`5.${index}`} id={`5.${index}`} value={i} className="slot" >{i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
-            {test[6].map((i, index) => (<button key={`6.${index}`} id={`6.${index}`} value={i} className="slot" >{i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
-            {test[7].map((i, index) => (<button key={`7.${index}`} id={`7.${index}`} value={i} className="slot" >{i === 0 ? "M":
-                i === 8 ? "H": "0"}</button>))}
+            {test[0].map((i, index) => (<button onClick={this.handleClick} key={`0.${index}`} id={`0.${index}`} value={i} className="slot" >
+                {i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
+            {test[1].map((i, index) => (<button onClick={this.handleClick} key={`1.${index}`} id={`1.${index}`} value={i} className="slot" >{i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
+            {test[2].map((i, index) => (<button onClick={this.handleClick} key={`2.${index}`} id={`2.${index}`} value={i} className="slot" >{i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
+            {test[3].map((i, index) => (<button onClick={this.handleClick} key={`3.${index}`} id={`3.${index}`} value={i} className="slot" >{i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
+            {test[4].map((i, index) => (<button onClick={this.handleClick} key={`4.${index}`} id={`4.${index}`} value={i} className="slot" >{i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
+            {test[5].map((i, index) => (<button onClick={this.handleClick} key={`5.${index}`} id={`5.${index}`} value={i} className="slot" >{i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
+            {test[6].map((i, index) => (<button onClick={this.handleClick} key={`6.${index}`} id={`6.${index}`} value={i} className="slot" >{i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
+            {test[7].map((i, index) => (<button onClick={this.handleClick} key={`7.${index}`} id={`7.${index}`} value={i} className="slot" >{i === 0 ? M:
+                i === 8 ? H: ""}</button>))}
         </div>
       </div>
       </>
--- src/Components/GameBoard/GameBoard.js
+++ src/Components/GameBoard/GameBoard.js
@@ -5,33 +5,19 @@ import ShipContainer from '../ShipContainer/ShipContainer';
 import './gameboard.css'
 
 class GameBoard extends Component{
-    state = {
-        // board:
-        // health:
-        // player_turn:
-        // active_game:false
-      };
-
-    // handlePlayerMove = (e) => {
-    //     e.preventDefault()
-    //   BattleshipApi.postMove(co)
-    //this.setState({
-
-    // })
-    //     
-    // }
-    handleAiMove = () => {
-        
-    }
-    renderImage(){
-        console.log(this.value)
-        if(this.value==1){
+    constructor() {
+        super();
+        this.playerMove = this.playerMove.bind(this);
+        this.state = {
+          idfromBoard: ''
+        };
+      }
+      //parent
+    playerMove(id){
+        this.setState({
+            idfromBoard: id
+        })
         }
-        if(this.value==0){
-        }
-        if(this.value==8){
-        }
-    }
     render(){
         const test = [
               [0, 0, 1, 5, 5, 5, 5, 5],
@@ -44,13 +30,16 @@ class GameBoard extends Component{
               [1, 3, 3, 3, 1, 1, 2, 2],
         ]
         console.log(test)
+        console.log("click id in game board", this.state.idfromBoard)
         return(<>
         <Header />
         <div className="gamePage">
         <div className="playerShips"><ShipContainer /></div>
             <div className="gameBoard">
                 <div className="player" id="player">
-                    <BoardRender test={test}/></div>
+                    <BoardRender 
+                    test={test} 
+                    playerMove={this.playerMove}/></div>
                 {/* <div className="opponent" id="opponent"><BoardRender test={test}/></div> */}
             </div>
             <div className="opponentShips"><ShipContainer /></div>
