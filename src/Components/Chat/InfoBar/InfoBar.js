086c174e227dbf51d369fa900970a87b2f88e3a6
Michael Oldacre <mloldacre@live.com>
2020-10-23 12:27:42 -0400

    Fixed package JSON

--- src/Components/Chat/InfoBar/InfoBar.js
+++ src/Components/Chat/InfoBar/InfoBar.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 // import './InfoBar.css';
 import closeIcon from '../Icons/closeIcon.png';
--- src/Components/Chat/Messages/Messages.js
+++ src/Components/Chat/Messages/Messages.js
@@ -1,7 +1,6 @@
 import React from 'react';
 // import './Messages.css';
 import Message from '../Message/Message';
-
 import ScrollToBottom from 'react-scroll-to-bottom';
 
 const Messages = ({ messages, name }) => (
